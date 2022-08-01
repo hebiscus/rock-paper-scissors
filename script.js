a48e6b9e47840af2dfe66f020e525721d904d90f
hebi <akikopoland@gmail.com>
2022-08-01 15:26:53 +0200

    first function

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    return choices[Math.floor(Math.random()*choices.length)]
+}
+console.log(getComputerChoice())
+
+function oneRound(playerSelection, computerSelection) {
+    
+}
