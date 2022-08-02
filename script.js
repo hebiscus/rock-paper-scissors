f32690d21f9eba849bd473994edec6b5c07836ff
hebi <akikopoland@gmail.com>
2022-08-02 21:58:45 +0200

    still figuring out 3rd function

--- script.js
+++ script.js
@@ -4,25 +4,35 @@ function getComputerChoice() {
 }
 
 let player = prompt ("This is a prompt box"); 
+let callCount = 0;
 
 function oneRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' & computerSelection == 'rock') {
+        callCount += 1;
         return "That's a tie!";
     } else if (playerSelection == 'rock' & computerSelection == 'scissors') {
+        callCount += 1;
         return "Player wins!";
     } else if (playerSelection == 'rock' & computerSelection == 'paper') {
+        callCount += 1;
         return "Computer wins!";
     } else if (playerSelection == 'paper' & computerSelection == 'paper') {
+        callCount += 1;
         return "That's a tie!";
     } else if (playerSelection == 'paper' & computerSelection == 'rock') {
+        callCount += 1;
         return "Player wins!";
     } else if (playerSelection == 'paper' & computerSelection == 'scissors') {
+        callCount += 1;
         return "Computer wins!";
     } else if (playerSelection == 'scissors' & computerSelection == 'scissors') {
+        callCount += 1;
         return "That's a tie!";
     } else if (playerSelection == 'scissors' & computerSelection == 'paper') {
+        callCount += 1;
         return "Player wins!";
     } else if (playerSelection == 'scissors' & computerSelection == 'rock') {
+        callCount += 1;
         return "Computer wins!";
     } else {
         return "Unavailable weapon"
@@ -31,13 +41,19 @@ function oneRound(playerSelection, computerSelection) {
 
 const playerSelection = player.toLowerCase();
 const computerSelection = getComputerChoice();
-/* console.log(oneRound(playerSelection, computerSelection)); */
+ console.log(oneRound(playerSelection, computerSelection)); 
+
 
 function game() {
-    let i = oneRound(playerSelection, computerSelection);
-    for (i = 1; i <= 5; i++) {
-        console.log(oneRound(playerSelection, computerSelection));
-    }
-}
+  /*  for (i = 1; i <= 5; i++) {
+       oneRound(playerSelection, computerSelection); 
+       console.log(oneRound(playerSelection, computerSelection,), i); */
+        oneRound(playerSelection, computerSelection);
+    } 
 
-console.log(game())
+
+
+
+
+
+console.log(game());
