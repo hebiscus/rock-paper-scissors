c07b6a01e487b5ece5cddc786f9dd10ff8e51004
hebi <akikopoland@gmail.com>
2022-08-02 12:29:34 +0200

    second function - oneRound

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 function getComputerChoice() {
     let choices = ['Rock', 'Paper', 'Scissors'];
-    return choices[Math.floor(Math.random()*choices.length)]
+    return choices[Math.floor(Math.random()*choices.length)].toLowerCase()
 }
-console.log(getComputerChoice())
+
+let player = prompt ("This is a prompt box"); 
 
 function oneRound(playerSelection, computerSelection) {
-    
+    if (playerSelection == 'rock' & computerSelection == 'rock') {
+        return "That's a tie!";
+    } else if (playerSelection == 'rock' & computerSelection == 'scissors') {
+        return "Player wins!";
+    } else if (playerSelection == 'rock' & computerSelection == 'paper') {
+        return "Computer wins!";
+    } else if (playerSelection == 'paper' & computerSelection == 'paper') {
+        return "That's a tie!";
+    } else if (playerSelection == 'paper' & computerSelection == 'rock') {
+        return "Player wins!";
+    } else if (playerSelection == 'paper' & computerSelection == 'scissors') {
+        return "Computer wins!";
+    } else if (playerSelection == 'scissors' & computerSelection == 'scissors') {
+        return "That's a tie!";
+    } else if (playerSelection == 'scissors' & computerSelection == 'paper') {
+        return "Player wins!";
+    } else if (playerSelection == 'scissors' & computerSelection == 'rock') {
+        return "Computer wins!";
+    } else {
+        return "Unavailable weapon"
+    }
 }
+
+const playerSelection = player.toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(oneRound(playerSelection, computerSelection));
+
