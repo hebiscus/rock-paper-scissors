12ffeba9d27cfa21aeea8d0580b0f441c2e2c3b9
hebi <akikopoland@gmail.com>
2022-08-03 18:29:37 +0200

    getting somewhere

--- script.js
+++ script.js
@@ -3,57 +3,52 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)].toLowerCase()
 }
 
-let player = prompt ("This is a prompt box"); 
-let callCount = 0;
+function getPlayerChoice() {
+    let player = prompt ("Choose your weapon");
+    return player.toLowerCase();
+}
 
-function oneRound(playerSelection, computerSelection) {
+/* let player = prompt ("This is a prompt box"); */
+
+function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' & computerSelection == 'rock') {
-        callCount += 1;
         return "That's a tie!";
     } else if (playerSelection == 'rock' & computerSelection == 'scissors') {
-        callCount += 1;
         return "Player wins!";
     } else if (playerSelection == 'rock' & computerSelection == 'paper') {
-        callCount += 1;
         return "Computer wins!";
     } else if (playerSelection == 'paper' & computerSelection == 'paper') {
-        callCount += 1;
         return "That's a tie!";
     } else if (playerSelection == 'paper' & computerSelection == 'rock') {
-        callCount += 1;
         return "Player wins!";
     } else if (playerSelection == 'paper' & computerSelection == 'scissors') {
-        callCount += 1;
         return "Computer wins!";
     } else if (playerSelection == 'scissors' & computerSelection == 'scissors') {
-        callCount += 1;
         return "That's a tie!";
     } else if (playerSelection == 'scissors' & computerSelection == 'paper') {
-        callCount += 1;
         return "Player wins!";
     } else if (playerSelection == 'scissors' & computerSelection == 'rock') {
-        callCount += 1;
         return "Computer wins!";
     } else {
         return "Unavailable weapon"
     }
 }
 
-const playerSelection = player.toLowerCase();
-const computerSelection = getComputerChoice();
- console.log(oneRound(playerSelection, computerSelection)); 
+score = "That's a tie" || "Player wins!" || "Computer wins!";
 
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+ console.log(playRound(playerSelection, computerSelection)); 
+ 
 
 function game() {
-  /*  for (i = 1; i <= 5; i++) {
-       oneRound(playerSelection, computerSelection); 
-       console.log(oneRound(playerSelection, computerSelection,), i); */
-        oneRound(playerSelection, computerSelection);
+     for (i = 1; i <= 5; i++) {
+        getPlayerChoice();
+        getComputerChoice();
+        playRound(playerSelection, computerSelection)
+       console.log(playRound(playerSelection, computerSelection));
     } 
 
-
-
-
-
+}
 
 console.log(game());
