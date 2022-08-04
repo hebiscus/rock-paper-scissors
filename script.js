0872c81f0b88471f5a1b1ca41a1553797a64e238
hebi <akikopoland@gmail.com>
2022-08-04 10:50:59 +0200

    so close omg

--- script.js
+++ script.js
@@ -8,24 +8,35 @@ function getPlayerChoice() {
     return player.toLowerCase();
 }
 
+
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' & computerSelection == 'rock') {
+        i -= 1;
         return "That's a tie!";
     } else if (playerSelection == 'rock' & computerSelection == 'scissors') {
+        playerScore += 1;
         return "Player wins!";
     } else if (playerSelection == 'rock' & computerSelection == 'paper') {
+        computerScore += 1;
         return "Computer wins!";
     } else if (playerSelection == 'paper' & computerSelection == 'paper') {
+        i -= 1;
         return "That's a tie!";
     } else if (playerSelection == 'paper' & computerSelection == 'rock') {
+        playerScore += 1;
         return "Player wins!";
     } else if (playerSelection == 'paper' & computerSelection == 'scissors') {
+        computerScore += 1;
         return "Computer wins!";
     } else if (playerSelection == 'scissors' & computerSelection == 'scissors') {
+        i -= 1;
         return "That's a tie!";
     } else if (playerSelection == 'scissors' & computerSelection == 'paper') {
+        playerScore += 1;
         return "Player wins!";
     } else if (playerSelection == 'scissors' & computerSelection == 'rock') {
+        computerScore += 1;        
         return "Computer wins!";
     } else {
         return "Unavailable weapon";
@@ -36,16 +47,26 @@ const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 /* console.log(playRound(playerSelection, computerSelection)); */
  
+let playerScore = 0;
+let computerScore = 0;
 
 function game() {
      for (i = 1; i <= 5; i++) {
-       /* getPlayerChoice(); */
         const playerSelection = getPlayerChoice();
-       /* getComputerChoice(); */
         const computerSelection = getComputerChoice();
        console.log(playRound(playerSelection, computerSelection));
-    } 
+    
 
-}
+    } 
+    if (playerScore > computerScore) {
+        return "Player has won the whole game!";
+    } else if (computerScore > playerScore) {
+        return "Computer has won the whole game!";
+    } else if (computerScore == playerScore) {
+        return "Game is tied!";
+    } else 
+        return "Uhoh, seems like you picked an unavailable weapon every round"; 
+    }
+
 
 console.log(game());
