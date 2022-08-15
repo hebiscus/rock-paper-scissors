065765ee8f8bdbc16dc646ce10c86dc010537094
hebi <akikopoland@gmail.com>
2022-08-15 17:43:31 +0200

    functions
    no styling yet

--- script.js
+++ script.js
@@ -4,45 +4,50 @@ function getComputerChoice() {
 }
 let playerScore = 0;
 let computerScore = 0;
-let gameRound = 0;
 
 const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' & computerSelection == 'rock') {
-        document.getElementById("roundScore").innerHTML += " " + "That's a tie!";
+        document.getElementById("roundScore").innerHTML = " " + "That's a tie!";
     } else if (playerSelection == 'rock' & computerSelection == 'scissors') {
         playerScore += 1;
-        document.getElementById("roundScore").innerHTML += "Player wins!";
+        document.getElementById("roundScore").innerHTML = "Player wins!";
     } else if (playerSelection == 'rock' & computerSelection == 'paper') {
         computerScore += 1;
-        document.getElementById("roundScore").innerHTML += " " + "Computer wins!";
+        document.getElementById("roundScore").innerHTML = " " + "Computer wins!";
     } else if (playerSelection == 'paper' & computerSelection == 'paper') {
-        document.getElementById("roundScore").innerHTML += " " + "That's a tie!";
+        document.getElementById("roundScore").innerHTML = " " + "That's a tie!";
     } else if (playerSelection == 'paper' & computerSelection == 'rock') {
         playerScore += 1;
-        document.getElementById("roundScore").innerHTML += " " + "Player wins!";
+        document.getElementById("roundScore").innerHTML = " " + "Player wins!";
     } else if (playerSelection == 'paper' & computerSelection == 'scissors') {
         computerScore += 1;
-        document.getElementById("roundScore").innerHTML += " " + "Computer wins!";
+        document.getElementById("roundScore").innerHTML = " " + "Computer wins!";
     } else if (playerSelection == 'scissors' & computerSelection == 'scissors') {
-        document.getElementById("roundScore").innerHTML += " " + "That's a tie!";
+        document.getElementById("roundScore").innerHTML = " " + "That's a tie!";
     } else if (playerSelection == 'scissors' & computerSelection == 'paper') {
         playerScore += 1;
-        document.getElementById("roundScore").innerHTML +=  " " + "Player wins!";
+        document.getElementById("roundScore").innerHTML =  " " + "Player wins!";
     } else if (playerSelection == 'scissors' & computerSelection == 'rock') {
         computerScore += 1;        
-        document.getElementById("roundScore").innerHTML += " " + "Computer wins!";
+        document.getElementById("roundScore").innerHTML = " " + "Computer wins!";
     } else {
-        document.getElementById("roundScore").innerHTML += " " + "Computer wins!";
+        document.getElementById("roundScore").innerHTML = " " + "Computer wins!";
     }
 }
 
 function game() {
     if (playerScore == 5 ) {
         document.getElementById("gameScore").textContent = document.getElementById("gameScore").textContent + " " + "You're so good... wowzers";
+        document.getElementById("rockButton").disabled = true;
+        document.getElementById("paperButton").disabled = true;
+        document.getElementById("scissorsButton").disabled = true;
     } else if (computerScore ==5) {
         document.getElementById("gameScore").textContent = document.getElementById("gameScore").textContent + " " + "Defeated by luck... how does it feel?";
+        document.getElementById("rockButton").disabled = true;
+        document.getElementById("paperButton").disabled = true;
+        document.getElementById("scissorsButton").disabled = true;
     } 
 }
 
@@ -54,7 +59,6 @@ let scissorsbutton = document.getElementById("scissorsButton");
 rockbutton.addEventListener("click", function(e) {
     if (playerScore <= 5 && computerScore <= 5) {
         playRound(e.target.value, getComputerChoice());
-        gameRound++;
         game();
     } else {
         return;
@@ -65,7 +69,6 @@ rockbutton.addEventListener("click", function(e) {
 paperbutton.addEventListener("click", function(e) {
     if (playerScore <= 5 && computerScore <= 5) {
         playRound(e.target.value, getComputerChoice());
-        gameRound++;
         game();
     } else {
         return;
@@ -76,7 +79,6 @@ paperbutton.addEventListener("click", function(e) {
 scissorsbutton.addEventListener("click", function(e) {
     if (playerScore <= 5 && computerScore <= 5) {
         playRound(e.target.value, getComputerChoice());
-        gameRound++;
         game();
     } else {
         return;
@@ -85,38 +87,3 @@ scissorsbutton.addEventListener("click", function(e) {
 
 
 
-    // if (playerScore > computerScore) {
-    //         return "Player has won the whole game!";
-    //     } else if (computerScore > playerScore) {
-    //         return "Computer has won the whole game!";
-    //     } else if (computerScore & playerScore == 0) {
-    //         return "damn you didn't even play :(";
-    //     } else if (computerScore == playerScore) {
-    //         return "Game is tied!"; 
-    //     } else 
-    //         return "Seems like game is tied!"; 
-
-
-
-// function getPlayerChoice() {
-//     let player = prompt ("Choose your weapon");
-//     return player.toLowerCase();
-// }
-
-// function game() {
-//      for (i = 1; i <= 5; i++) {
-//         const playerSelection = getPlayerChoice();
-//         const computerSelection = getComputerChoice();
-//        console.log(playRound(playerSelection, computerSelection));
-//     } 
-//     if (playerScore > computerScore) {
-//         return "Player has won the whole game!";
-//     } else if (computerScore > playerScore) {
-//         return "Computer has won the whole game!";
-//     } else if (computerScore & playerScore == 0) {
-//         return "damn you didn't even play :(";
-//    /* } else if (computerScore == playerScore) {
-//         return "Game is tied!"; */
-//     } else 
-//         return "Seems like game is tied!"; 
-//     }
